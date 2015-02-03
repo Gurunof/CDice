43f64e19ebf48d83801f3fa5a6ed1dbf775d0ac6
Gurunof <gurunof@gmail.com>
2015-02-03 12:08:41 +0300

    Initial Commit

---
+++ CDice/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CDice
+//
+//  Created by Dmitry on 2/3/15.
+//  Copyright (c) 2015 free. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
